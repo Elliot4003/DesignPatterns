d14fbeee3ed5b1e33612f5a364362f4eef5d8bbd
GROUPEADEQUAT\elliot.crozet <elliot.crozet@groupeadequat.fr>
2026-02-23 08:53:25 +0100

    removed unused property

--- BehavioralPatterns/ObserverPattern/Subjects/MoviePublisher.cs
+++ BehavioralPatterns/ObserverPattern/Subjects/MoviePublisher.cs
@@ -7,17 +7,14 @@ public class MoviePublisher : ISubject
 {
     private event Action<ISubject>? SubjectNotification; // Délégué permettant d'impacter tous les observers abonnés
     public MovieTypesEnum Type { get; private set; } = MovieTypesEnum.Undefined;
-    private readonly List<IObserver> _observers = [];
 
     public void Attach(IObserver observer)
     {
-        _observers.Add(observer);
         SubjectNotification += observer.Update; // Abonner l'observer
     }
 
     public void Detach(IObserver observer)
     {
-        _observers.Remove(observer);
         SubjectNotification -= observer.Update; // Désabonner l'observer
     }
 
